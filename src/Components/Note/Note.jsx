d21f071bf0d9373c5f7c72c42f27d4aa41f12c5d
Chris <csjohnson79@icloud.com>
2020-03-31 17:06:39 -0600

    created App.css and set up App, Header, Footer, and Note components

---
+++ src/Components/Note/Note.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '../../App.css';
+
+class Note extends Component {
+    render() {
+        return(
+            <div className="note">
+                <h1>Note</h1>
+                <p>Content</p>
+            </div>
+        );
+    }
+}
+
+export default Note;
