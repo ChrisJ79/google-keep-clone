8ddeeba805b0cae112a1e9abe90305469607df39
Chris <csjohnson79@icloud.com>
2020-04-02 15:37:00 -0600

    Mapped through Notes array into Note component on App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Header from './Components/Header/Header';
 import Note from './Components/Note/Note';
 import Footer from './Components/Footer/Footer';
+import Notes from './notes';
 import './App.css';
+
+
+
 
 const App = () => (
   <div className="App">
     <Header />
-    <Note 
-      title="Note Title"
-      content="Note Content"
-    />
+    {Notes.map(
+      function createNotes(noteItem) {
+        return (
+        <Note
+          key={noteItem.id}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+        )
+      } 
+    )}
     <Footer />
   </div>
 )
